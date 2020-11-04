0bdce8a8f6982f3a8355663bba8b0e3074f27d26
Awuru James <mondayjamesawuru10@mail.com>
2020-11-04 01:29:34 +0100

    first commit

---
+++ polymorphAss1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace polymorphAss1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OvernightPackage overnightPackage = new OvernightPackage("Abuja", "Lagos", 5500M);
+            TwoDaysPackage twoDaysPackage = new TwoDaysPackage("Jos", "Kaduna", 8500M);
+
+            // Console.WriteLine($"{overnightPackage}");
+            // Console.WriteLine($"{twoDaysPackage}");
+
+            Console.WriteLine("\n Packages processed polymorphically...\n");
+            Package[] packages = new Package[2] { overnightPackage, twoDaysPackage };
+
+            foreach (Package package in packages)
+            {
+                Console.WriteLine($"{package}");
+            }
+
+
+
+
+        }
+    }
+}
